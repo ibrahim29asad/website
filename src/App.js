3dcda765509efcd57eea7c425c4d36c6e11c0f32
ibrahim29asad <ibrahim.asad2019@gmail.com>
2024-01-24 22:46:28 -0700

    Fixed Background and moved Profiles to the Right
    Might add Resume to the left or Languages Known, need to change it so it changes dynamically, as well as add technologies used

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Profile from '../src/images/profile.jpg'
-import GitHub from '../src/images/github.jpg'
-import LinkedIn from '../src/images/Linkedin.jpg'
-import LeetCode from '../src/images/leetcode.jpg'
-import Instagram from '../src/images/instagram.jpg'
-import TikTok from '../src/images/tiktok.jpg'
 import ImageGallery from './ImageGallery';
+import Socials from './Socials';
 
 function App() {
   return (
 
-    <div className="container">
-      {/* <div className="side-section"></div> */}
+    <div className="container"  >
+      <div className="side-section"></div>
       
       <div className="main-section">
       <div className='HeaderName'>
@@ -32,42 +28,23 @@ function App() {
       <p style={{ textDecoration: 'underline' }}>.posts.</p>
       <ImageGallery />
       <p style={{ textDecoration: 'underline' }}>.technology.</p>
+
       </div>
       </div>
 
 
 
 
-      <div className="side-section">
-  
+      <div className="side-section-left">
+      {/* display: flex;
+      justify-content: center; */}
+      <img src={Profile} className="Profile-Pic" alt="Profile" />      
+      <div className='Sidebar-HeaderName'>
+      <p style={{ textDecoration: 'underline' }}> Skills and Languages Known:</p>
+      <Socials />
+      </div>
+     
       
-      <div className='SocialText'>
-      <img src={Profile} className="Profile-Pic" alt="Profile" />
-
-
-      <div className='SocialText'>
-      <p>Socials: </p>
-      <p><img src={GitHub} className="Socials-Pic" alt="Profile" />
-      <a href="https://github.com/ibrahim29asad">github </a> -  </p>
-      https://github.com/ibrahim29asad
-
-      <p><img src={LinkedIn} className="Socials-Pic" alt="Profile" />
-      <a href="https://www.linkedin.com/in/iibrahimasad/">Linkedin </a> -  </p>
-      https://www.linkedin.com/in/iibrahimasad/
-
-      <p><img src={LeetCode} className="Socials-Pic" alt="Profile" />
-      <a href="https://leetcode.com/user1833qc/">leetcode </a> -  </p>
-      https://leetcode.com/user1833qc/
-
-      <p><img src={Instagram} className="Socials-Pic" alt="Profile" />
-      <a href="https://www.instagram.com/ibrahim29asad/">instagram </a>  </p>
-      
-
-      <p><img src={TikTok} className="Socials-Pic" alt="Profile" />
-      <a href="https://www.tiktok.com/@ibby_asd?_t=8jJfNXcJmgn&_r=1">tiktok </a> </p>
-    
-      </div>
-      </div>
       </div>
 
 
---
+++ src/Socials.js
@@ -0,0 +1,41 @@
+import './App.css';
+import React from 'react';
+import GitHub from '../src/images/github.jpg'
+import LinkedIn from '../src/images/Linkedin.jpg'
+import LeetCode from '../src/images/leetcode.jpg'
+import Instagram from '../src/images/instagram.jpg'
+import TikTok from '../src/images/tiktok.jpg'
+import ImageGallery from './ImageGallery';
+
+
+const Socials = () => {
+  
+  return (
+   
+    <div className='SocialText'>
+    <p>Socials: </p>
+    <p><img src={GitHub} className="Socials-Pic" alt="Profile" />
+    <a href="https://github.com/ibrahim29asad">github </a> -  </p>
+    https://github.com/ibrahim29asad
+
+    <p><img src={LinkedIn} className="Socials-Pic" alt="Profile" />
+    <a href="https://www.linkedin.com/in/iibrahimasad/">Linkedin </a> -  </p>
+    https://www.linkedin.com/in/iibrahimasad/
+
+    <p><img src={LeetCode} className="Socials-Pic" alt="Profile" />
+    <a href="https://leetcode.com/user1833qc/">leetcode </a> -  </p>
+    https://leetcode.com/user1833qc/
+
+    <p><img src={Instagram} className="Socials-Pic" alt="Profile" />
+    <a href="https://www.instagram.com/ibrahim29asad/">instagram </a>  </p>
+    
+
+    <p><img src={TikTok} className="Socials-Pic" alt="Profile" />
+    <a href="https://www.tiktok.com/@ibby_asd?_t=8jJfNXcJmgn&_r=1">tiktok </a> </p>
+  
+    </div>
+    
+  );
+};
+
+export default Socials;
